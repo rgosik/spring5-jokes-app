bddd7dcb577857a076f191f5b4e15203881dbbac
Rafal <grafal96@gmail.com>
2020-03-04 22:51:46 +0100

    first commit

---
+++ src/main/java/org/jokesapp/springjokesapp/services/JokesService.java
@@ -0,0 +1,11 @@
+package org.jokesapp.springjokesapp.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesService {
+
+    public String getRandomQuote(){
+        return guru.springframework.norris.chuck.ChuckNorrisQuotes.getRandomQuote;
+    }
+}
